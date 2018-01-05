89ac25dbf2ed0ab8ba090b0e212e6dd845145c32
iliguo <for_liguo@163.com>
2018-01-05 11:20:57 +0800

    test git

---
+++ spring-cloud-config-eureka-bus/spring-cloud-config-client-x/src/main/java/com/neo/web/HelloController.java
@@ -0,0 +1,27 @@
+package com.neo.web;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RefreshScope
+class HelloController {
+
+    @Value("${neo.hello}")
+    private String hello;
+
+    @RequestMapping("/hello")
+    public String from() {
+        return this.hello;
+    }
+
+//    @Value("${my-config.appName}")
+//    private String appName;
+//
+//    @RequestMapping("/hello")
+//    public String from() {
+//        return this.appName;
+//    }
+}
